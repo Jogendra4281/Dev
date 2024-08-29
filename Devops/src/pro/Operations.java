9cbd5d13a38b6240df52f39f638b9328624c55f2
AdmiN <AdmiN@ITWS0041>
2024-08-29 10:28:59 +0530

    commit1

---
+++ Devops/src/pro/Operations.java
@@ -0,0 +1,28 @@
+package pro;
+import java.util.*;
+public class Operations {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//Opertions op = new Operations();
+		Scanner sc = new Scanner(System.in); 
+		int a = sc.nextInt(),b = sc.nextInt();
+		sum(a,b);
+		sub(a,b);
+		mul(a,b);
+	}
+	public static void sum(int a,int b)
+	{
+		// this should be done sravani
+	}
+	public static void sub(int a,int b)
+	{
+		// this should be done sai
+	}
+	public static void mul(int a,int b)
+	{
+		//this is done by me
+		System.out.println(a*b);
+	}
+
+}
